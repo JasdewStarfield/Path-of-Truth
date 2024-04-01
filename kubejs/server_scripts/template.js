393d593b5ee4a69cdf3ae080f1f49898dbf9906c
MidnightPigeon <chen_xs_021105@126.com>
2024-04-01 18:46:15 +0800

    模组添加与基本配方改动

--- kubejs/server_scripts/template.js
+++ kubejs/server_scripts/template.js
@@ -17,3 +17,32 @@ event.shaped(Item.of('名字',数量),
     }
 )
 */
+
+/*
+植物魔法系列配方记录：
+onEvent("recipes", event => {
+    event.remove({ output: "botania:livingrock" })
+ 
+    event.recipes.botania.mana_infusion("minecraft:acacia_boat", "minecraft:acacia_door", 200, "minecraft:acacia_log")
+    
+    event.recipes.botania.elven_trade(["minecraft:acacia_boat"], "minecraft:diamond")
+    event.recipes.botania.elven_trade(["minecraft:acacia_boat", "minecraft:acacia_button"], ["minecraft:diamond_block", "minecraft:gold_ingot"])
+    
+    event.recipes.botania.pure_daisy("minecraft:acacia_button", "minecraft:acacia_leaves")
+    event.recipes.botania.pure_daisy("minecraft:acacia_button", "minecraft:stone", 1)
+ 
+    event.recipes.botania.brew("kubejs:torrent", ["minecraft:acacia_boat"])
+ 
+    event.recipes.botania.petal_apothecary("minecraft:acacia_boat", ["minecraft:acacia_button"])
+ 
+    event.recipes.botania.runic_altar("minecraft:acacia_boat", ["minecraft:acacia_button", "minecraft:acacia_door"], 5000)
+ 
+    event.recipes.botania.terra_plate("minecraft:acacia_boat", ["minecraft:acacia_button", "minecraft:acacia_door"], 5000000)
+ 
+    event.recipes.botania.orechid("minecraft:acacia_button", "minecraft:acacia_fence", 1)
+ 
+    event.recipes.botania.orechid_ignem("minecraft:acacia_leaves", "minecraft:acacia_fence_gate", 1)
+ 
+    event.recipes.botania.marimorphosis("minecraft:acacia_door", "minecraft:acacia_fence_gate", 1, ["plains"], 10)
+})
+*/
