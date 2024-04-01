468878c4681eb5014c8900d9281603ac99760de5
MidnightPigeon <chen_xs_021105@126.com>
2024-04-01 20:09:20 +0800

    修改配方，任务，删除邪恶工艺残余

--- kubejs/server_scripts/bontania.js
+++ kubejs/server_scripts/bontania.js
@@ -3,7 +3,7 @@
 ServerEvents.recipes(event => {
     //修改白雏菊合成配方
     event.remove({ output: 'botania:pure_daisy' })
-    event.recipes.botania.petal_apothecary('botania:pure_daisy', ['botania:white_petal','irons_spellbooks:arcane_essence','#forge:dusts/tin'])
+    event.recipes.botania.petal_apothecary('botania:pure_daisy', ['botania:white_petal','irons_spellbooks:arcane_essence','#forge:ingots/tin'])
 
 
 })
