6d4e1411c198b8e00d41b74cfd507fc55dfa5bb1
MidnightPigeon <chen_xs_021105@126.com>
2024-04-06 20:40:43 +0800

    任务更新

--- kubejs/server_scripts/bloodmagic.js
+++ kubejs/server_scripts/bloodmagic.js
@@ -1,8 +1,14 @@
 //此文件提供血魔法系列修改
 ServerEvents.recipes(event => {
     //修改血祭坛配方
+    //底层青铜，两侧活石，中间高炉砖
     event.replaceInput({mod:'bloodmagic',output:'bloodmagic:altar'},
     'minecraft:gold_ingot', 'kubejs:bronze_ingot' )
+    event.replaceInput({mod:'bloodmagic',output:'bloodmagic:altar'},
+    '#balm:stones', 'botania:livingrock' )
+    event.replaceInput({mod:'bloodmagic',output:'bloodmagic:altar'},
+    'minecraft:furnace', 'immersiveengineering:blastbrick' )
+
 
     
 })
