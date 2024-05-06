092af4d2e67431c27fc173544c9308fd31d9c84e
MysteriousEnder <g82425813@163.com>
2024-05-07 00:32:26 +0800

    Tech Finale 2.0

--- kubejs/server_scripts/tech_endgame.js
+++ kubejs/server_scripts/tech_endgame.js
@@ -15,14 +15,49 @@ ServerEvents.recipes(event => {
   //移除一些原有配方
   event.remove({ id: 'createchromaticreturn:bedrock_shard_crushing' })
   event.remove({ id: 'createchromaticreturn:chromatic_compound_recipe' })
+  event.remove({ id: 'createchromaticreturn:andesite_component_recipe' })
+  event.remove({ id: 'createchromaticreturn:brass_component_recipe' })
 
   //环境枪
   event.shapeless(
     Item.of('create:handheld_worldshaper',1),
     [
-      'create:creative_crate',
-      'create:creative_fluid_tank',
-      'create:creative_motor'
+      'createchromaticreturn:industrium_charm',
+      'createchromaticreturn:shadow_charm',
+      'createchromaticreturn:refined_charm',
+      'createchromaticreturn:multiplite_charm',
+      'createchromaticreturn:silkstrum_charm',
+      'createchromaticreturn:antiplite_charm'
     ]
   )
+
+  //工业终极锭
+  event.remove({ id: 'createchromaticreturn:industrium_recipe' })
+  event.remove({ id: 'createchromaticreturn:durasteel_recipe' })
+  event.remove({ id: 'createchromaticreturn:industrium_book_recipe' })
+  event.remove({ id: 'createchromaticreturn:durasteel_book_recipe' })
+
+  event.recipes.create.mixing(['createchromaticreturn:industrium_ingot'], ['createchromaticreturn:shadow_steel','create:deployer','create:mechanical_arm','immersiveengineering:powerpack','tfmg:large_radial_engine']).superheated()
+  event.recipes.create.sequenced_assembly([
+    Item.of('createchromaticreturn:industrium_book').withChance(10.0),
+    Item.of('book').withChance(90.0)
+    ], 'immersiveengineering:manual', [
+    event.recipes.createDeploying('kubejs:incomplete_industrium_book', ['kubejs:incomplete_industrium_book', '#forge:plates/plastic']),
+    event.recipes.createFilling('kubejs:incomplete_industrium_book', ['kubejs:incomplete_industrium_book', Fluid.of('create_enchantment_industry:hyper_experience',500)]),
+    event.recipes.createDeploying('kubejs:incomplete_industrium_book', ['kubejs:incomplete_industrium_book', 'createchromaticreturn:industrium_ingot']),
+    event.recipes.createFilling('kubejs:incomplete_industrium_book', ['kubejs:incomplete_industrium_book', Fluid.of('create_enchantment_industry:ink',500)]),
+    event.recipes.createDeploying('kubejs:incomplete_industrium_book', ['kubejs:incomplete_industrium_book', '#forge:plates/plastic'])
+]).transitionalItem('kubejs:incomplete_industrium_book').loops(10)
+
+  event.recipes.create.compacting(['createchromaticreturn:durasteel_ingot'], ['createchromaticreturn:shadow_steel','#forge:storage_blocks/froststeel','#forge:storage_blocks/steel','#forge:storage_blocks/iron','#forge:storage_blocks/cast_iron','#blue_skies:storage_blocks/horizonite','#forge:storage_blocks/netherite']).superheated()
+  event.recipes.create.sequenced_assembly([
+    Item.of('createchromaticreturn:durasteel_book').withChance(10.0),
+    Item.of('book').withChance(90.0)
+    ], 'immersiveengineering:manual', [
+    event.recipes.createDeploying('kubejs:incomplete_durasteel_book', ['kubejs:incomplete_durasteel_book', '#forge:plates/obsidian']),
+    event.recipes.createFilling('kubejs:incomplete_durasteel_book', ['kubejs:incomplete_durasteel_book', Fluid.of('create_enchantment_industry:hyper_experience',500)]),
+    event.recipes.createDeploying('kubejs:incomplete_durasteel_book', ['kubejs:incomplete_durasteel_book', 'createchromaticreturn:durasteel_ingot']),
+    event.recipes.createFilling('kubejs:incomplete_durasteel_book', ['kubejs:incomplete_durasteel_book', Fluid.of('create_enchantment_industry:ink',500)]),
+    event.recipes.createDeploying('kubejs:incomplete_durasteel_book', ['kubejs:incomplete_durasteel_book', '#forge:plates/obsidian'])
+]).transitionalItem('kubejs:incomplete_durasteel_book').loops(10)
 })
---
+++ kubejs/startup_scripts/finale.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('item', (event) => {
+  //书
+  event.create("incomplete_industrium_book", 'create:sequenced_assembly').displayName('Incomplete Industrium Infused Book')
+  event.create("incomplete_durasteel_book", 'create:sequenced_assembly').displayName('Incomplete Durasteel Infused Book')
+})
