cfffdf55220725a1b73965527e9442cfc5e059ed
MysteriousEnder <g82425813@163.com>
2024-05-22 19:56:42 +0800

    Respawn Reworked
    Change the config of Respawn Obelisks mod
    Add a obelisk base block that is unbreakable, and provides infinite charge for the obelisk
    Remove Carry On mod

--- kubejs/startup_scripts/finale.js
+++ kubejs/startup_scripts/finale.js
@@ -3,3 +3,18 @@ StartupEvents.registry('item', (event) => {
   event.create("incomplete_industrium_book", 'create:sequenced_assembly').displayName('Incomplete Industrium Infused Book')
   event.create("incomplete_durasteel_book", 'create:sequenced_assembly').displayName('Incomplete Durasteel Infused Book')
 })
+
+StartupEvents.registry('block', (event) => {
+  //重生碑底座
+  event.create('obelisk_base')
+  .displayName('Obelisk Base')
+  .soundType("copper")
+  .mapColor("metal")
+  .hardness(-1.0)
+  .resistance(-1.0)
+  .requiresTool(true)
+  .opaque(true)
+  .fullBlock(true)
+  .renderType('solid')
+  .textureAll('kubejs:block/obelisk_base')
+})
