58776d9876732e63101d93cdc84fb0f73cf013a1
JackyBlackson <Jacky_Blackson@outlook.com>
2024-05-28 19:50:53 +0800

    init: create index.html to make github pages

--- kubejs/client_scripts/tooltips.js
+++
@@ -1,5 +0,0 @@
-ItemEvents.tooltip(e => {
-    e.add(["kubejs:obelisk_base"], [Text.aqua({translate:'item.kubejs.obelisk_base.desc1'}),Text.red({translate:'item.kubejs.obelisk_base.desc2'})])
-    e.add(["kubejs:disc_fragment_yggdrasil"], [Text.aqua({translate:'item.kubejs.disc_fragment_yggdrasil.desc1'}),Text.red({translate:'item.kubejs.disc_fragment_yggdrasil.desc2'})])
-    e.add(["kubejs:incomplete_music_disc_pluto_nocturnus"], [Text.gray({translate:'item.kubejs.incomplete_music_disc_pluto_nocturnus.desc1'}),Text.gray({translate:'item.kubejs.incomplete_music_disc_pluto_nocturnus.desc2'})])
-})
