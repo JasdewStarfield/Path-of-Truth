d7e979811bda9f98b0bbd7e5bcca6be253125af4
JasdewStarfield <mysteriousender@163.com>
2024-06-11 01:08:40 +0800

    Experimental Create Tooltips
    实验性添加机械动力风格物品描述（使用机械动力自己的方法）

---
+++ kubejs/startup_scripts/create_tooltips.js
@@ -0,0 +1,29 @@
+//实验性添加机械动力形式的tooltips支持
+const ItemDescription = Java.loadClass("com.simibubi.create.foundation.item.ItemDescription$Modifier")
+const TooltipModifier = Java.loadClass("com.simibubi.create.foundation.item.TooltipModifier")
+const Palette = Java.loadClass("com.simibubi.create.foundation.item.TooltipHelper$Palette")
+
+const CreateTooltipsItems = [
+    "immersiveengineering:hammer"
+]
+
+//下面是格式，写在assets/kubejs/lang/zh_cn.json（或者英文的就在en_us.json）
+/*
+{
+  "item.{namespace}.{item}.tooltip": "114514(随便写点什么，告诉create这个东西存在tooltip)",
+  "item.{namespace}.{item}.tooltip.summary": "按shift显示的内容",
+  "item.{namespace}.{item}.tooltip.condition1": "条件",
+  "item.{namespace}.{item}.tooltip.behaviour1": "该条件下的行为，同理可添加condition2和behaviour2等等",
+  "item.{namespace}.{item}.tooltip.control1": "按control显示的内容，一般用来写按键操作",
+  "item.{namespace}.{item}.tooltip.action1": "对应操作的行为，同样可以写多条"
+}
+*/
+
+ClientEvents.init(event => {
+    CreateTooltipsItems.forEach((id) => 
+            TooltipModifier.REGISTRY.register(
+            id,
+            new ItemDescription(id, Palette.STANDARD_CREATE)
+            )
+    )
+})
