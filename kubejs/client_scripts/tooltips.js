7c6953ede8e7e00d69453e01fee09946ad0478b8
JasdewStarfield <mysteriousender@163.com>
2024-06-28 22:30:03 +0800

    模组添加，适配及更多
    增加了Collector's Reap（农夫乐事附属模组），Mo'Enchantments（更多附魔的模组）并进行了相关适配
    削弱钻石装备：大幅降低了其耐久值，但是其它属性不变
    雪球现在可以堆叠至64个了
    卷心菜卷的另一原料现在只能使用荤菜了（为了符合营养元素）

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -2,4 +2,21 @@ ItemEvents.tooltip(e => {
     e.add(["kubejs:obelisk_base"], [Text.aqua({translate:'item.kubejs.obelisk_base.desc1'}),Text.red({translate:'item.kubejs.obelisk_base.desc2'})])
     e.add(["kubejs:disc_fragment_yggdrasil"], [Text.aqua({translate:'item.kubejs.disc_fragment_yggdrasil.desc1'}),Text.red({translate:'item.kubejs.disc_fragment_yggdrasil.desc2'})])
     e.add(["kubejs:incomplete_music_disc_pluto_nocturnus"], [Text.gray({translate:'item.kubejs.incomplete_music_disc_pluto_nocturnus.desc1'}),Text.gray({translate:'item.kubejs.incomplete_music_disc_pluto_nocturnus.desc2'})])
+
+    const diamondGears = [
+        'minecraft:diamond_helmet',
+        'minecraft:diamond_chestplate',
+        'minecraft:diamond_leggings',
+        'minecraft:diamond_boots',
+        'minecraft:diamond_sword',
+        'minecraft:diamond_pickaxe',
+        'minecraft:diamond_axe',
+        'minecraft:diamond_shovel',
+        'minecraft:diamond_hoe',
+        'farmersdelight:diamond_knife',
+        'nethersdelight:diamond_machete'
+    ]
+    diamondGears.forEach((id) =>
+        e.add([id], [Text.red({translate:'item.kubejs.diamond_gears.desc'})])
+    )
 })
