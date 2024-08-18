60cb8e3876bc7e145a669773cb6e9b04f28b7d96
MidnightPigeon <139368780+MidnightPigeon@users.noreply.github.com>
2024-08-18 23:52:32 +0800

    Revert "对第六章进行一个过河拆桥（"

--- kubejs/client_scripts/endgame.js
+++
@@ -1,5 +0,0 @@
-JEIEvents.removeRecipes((e) => {
-  e.remove("create:mechanical_crafting", "minecraft:endgame1");
-  e.remove("create:mechanical_crafting", "minecraft:endgame2");
-  e.remove("create:mechanical_crafting", "minecraft:endgame_test");
-});
