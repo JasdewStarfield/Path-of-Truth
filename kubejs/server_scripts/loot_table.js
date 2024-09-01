5a50ebca2335799bb1ffdf7b2cc87d4ce33e5982
JasdewStarfield <mysteriousender@163.com>
2024-09-01 15:41:15 +0800

    Mods Update and More
    -更新了一些模组（已知QuestAdditions的1.4.5版本会导致末影人相关崩溃，暂不更新）。
    -将可能出现在箱子战利品中的下界合金锭改为远古残骸。
    -移除了炙铁锭、霜钢锭的正常熔炼配方，改为使用高炉/雪帽熔炉制作。
    -更换了动态光源模组，适配新加入的机械动力动态光源。

--- kubejs/server_scripts/loot_table.js
+++ kubejs/server_scripts/loot_table.js
@@ -4,6 +4,7 @@ LootJS.modifiers((event) => {
     event.addLootTypeModifier(LootType.CHEST).replaceLoot("iron_block", "raw_iron_block", true)
     event.addLootTypeModifier(LootType.CHEST).replaceLoot("iron_ingot", "raw_iron", true)
     event.addLootTypeModifier(LootType.CHEST).replaceLoot("iron_nugget", "minecraft:copper_nugget", true)
+    event.addLootTypeModifier(LootType.CHEST).replaceLoot("netherite_ingot", "ancient_debris", true)
     event.addLootTypeModifier(LootType.ENTITY).replaceLoot("iron_ingot", "raw_iron", true)
     event.addLootTableModifier(/.*/).removeLoot("netherite_ingot")
     event.addBlockLootModifier("#minecraft:replaceable_by_trees").removeLoot("minecraft:wheat_seeds")
