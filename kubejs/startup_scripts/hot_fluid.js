e126acaf729fcf92ba8cf2da21fb34d9cfae4051
JasdewStarfield <mysteriousender@163.com>
2025-03-26 23:20:02 +0800

    Sulfuric Acid Damage Source
    -硫酸的伤害类型现在正确了。

--- kubejs/startup_scripts/hot_fluid.js
+++ kubejs/startup_scripts/hot_fluid.js
@@ -13,7 +13,6 @@ ForgeEvents.onEvent('net.minecraftforge.event.entity.living.LivingEvent$LivingTi
 
 let ResourceKey = Java.loadClass('net.minecraft.resources.ResourceKey')
 let Registries = Java.loadClass('net.minecraft.core.registries.Registries')
-//let acidDamageSource = global.getFromRegistry(Registries.DAMAGE_TYPE, "immersiveengineering:acid")
 
 /**
 * 
@@ -21,7 +20,7 @@ let Registries = Java.loadClass('net.minecraft.core.registries.Registries')
 */
 global.entitytick = event => {
   const { entity } = event
-  
+  let acidDamageSource = DamageSource(global.getFromRegistry(Registries.DAMAGE_TYPE, "immersiveengineering:acid"))
   hotFluids.forEach((fluidName) => {
     if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
       entity.lavaHurt()
@@ -29,15 +28,13 @@ global.entitytick = event => {
   })
   acidFluids.forEach((fluidName) => {
     if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
-      entity.attack(/*acidDamageSource, */2)
+      entity.attack(acidDamageSource, 2)
     }
   })
 }
 
-/*
 global.getFromRegistry = (regKey, id) => {
   var Level = Utils.server.overworld()
   var registry = Level.registryAccess().registryOrThrow(regKey)
   return registry.getHolderOrThrow(ResourceKey.create(regKey, id))
 }
-*/
