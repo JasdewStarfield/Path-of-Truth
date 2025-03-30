13aa169b5612a39b241e8f8c9dc9c5c55703667e
JasdewStarfield <mysteriousender@163.com>
2025-03-30 23:40:28 +0800

    Additional Fix
    -增加了更多流体交互。
    -修复了工业里程碑任务。
    -修复几个任务错误。
    -更新模组。

--- kubejs/startup_scripts/hot_fluid.js
+++ kubejs/startup_scripts/hot_fluid.js
@@ -3,10 +3,29 @@ const hotFluids = [
   "kubejs:molten_steel",
   "kubejs:molten_slag",
   "kubejs:molten_za",
-  "kubejs:molten_netherite"
+  "kubejs:molten_netherite",
+  "kubejs:molten_enriched_uranium",
+  "createnuclear:uranium"
 ]
 const acidFluids = [
   "vintageimprovements:sulfuric_acid"
+]
+const flammableFluids = [
+  "createdieselgenerators:diesel",
+  "createdieselgenerators:gasoline",
+  "kubejs:lpg",
+  "kubejs:lubricant",
+]
+const slownessFluids = [
+  "kubejs:heavy_oil",
+  "kubejs:liquid_plastic",
+  "createdieselgenerators:crude_oil"
+]
+const slipperyFluids = [
+  "kubejs:lubricant"
+]
+const radiationFluids = [
+  "kubejs:molten_enriched_uranium"
 ]
 
 ForgeEvents.onEvent('net.minecraftforge.event.entity.living.LivingEvent$LivingTickEvent', event => {
@@ -31,6 +50,26 @@ global.entitytick = event => {
   acidFluids.forEach((fluidName) => {
     if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
       entity.attack(acidDamageSource, 2)
+    }
+  })
+  flammableFluids.forEach((fluidName) => {
+    if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
+      entity.potionEffects.add("immersiveengineering:flammable", 100, 1, false, true)
+    }
+  })
+  slownessFluids.forEach((fluidName) => {
+    if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
+      entity.potionEffects.add("minecraft:slowness", 20, 3, false, true)
+    }
+  })
+  slipperyFluids.forEach((fluidName) => {
+    if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
+      entity.potionEffects.add("immersiveengineering:slippery", 200, 0, false, true)
+    }
+  })
+  radiationFluids.forEach((fluidName) => {
+    if (entity.isInFluidType(Fluid.of(fluidName).fluid.fluidType)) {
+      entity.potionEffects.add("createnuclear:radiation", 80, 0, false, true)
     }
   })
 }
