478e9ca82faf45b40c14381ac83a6ed1cbc7624a
YukkuriC <799433638@qq.com>
2025-05-22 22:17:49 +0800

    bugfix: extract settings later in game

--- kubejs/client_scripts/client_cave_limit.js
+++ kubejs/client_scripts/client_cave_limit.js
@@ -1,10 +1,14 @@
 {
-    let settings = Java.loadClass('xaero.map.WorldMap').settings
-    let fCaveModeAllowed = settings.class.getDeclaredField('caveMapsAllowed')
-    fCaveModeAllowed.setAccessible(true)
+    let settings = null
+    let fCaveModeAllowed = null
 
     PlayerEvents.tick(e => {
         if (e.player.age % 30) return
+        if (!fCaveModeAllowed) {
+            settings = Java.loadClass('xaero.map.WorldMap').settings
+            fCaveModeAllowed = settings.class.getDeclaredField('caveMapsAllowed')
+            fCaveModeAllowed.setAccessible(true)
+        }
         let dimKey = String(e.level.dimensionKey.location())
         let whiteList = /* player.creative || */ dimKey == 'minecraft:the_nether' || dimKey.startsWith('undergarden')
         fCaveModeAllowed.set(settings, whiteList)
