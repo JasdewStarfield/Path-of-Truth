a081ebfe9e1265f7c2af9b49b1a82d33264e1689
YukkuriC <799433638@qq.com>
2025-06-06 21:55:34 +0800

    enable cave sonar

--- kubejs/client_scripts/client_cave_limit.js
+++ kubejs/client_scripts/client_cave_limit.js
@@ -11,6 +11,8 @@
         }
         let dimKey = String(e.level.dimensionKey.location())
         let whiteList = /* player.creative || */ dimKey == 'minecraft:the_nether' || dimKey.startsWith('undergarden')
+        if (!whiteList && e.player.inventory.find('kubejs:sonic_mechanism') >= 0) whiteList = true
+        e.player.enderChestInventory
         fCaveModeAllowed.set(settings, whiteList)
     })
 }
