81a811a05227afab3da4758e54462b9609c266cf
YukkuriC <799433638@qq.com>
2025-06-06 22:31:08 +0800

    君子协定版实体雷达

--- kubejs/client_scripts/client_cave_limit.js
+++ kubejs/client_scripts/client_cave_limit.js
@@ -1,6 +1,8 @@
 {
     let settings = null
     let fCaveModeAllowed = null
+    let settingsMinimap = null
+    let minimapOptions = Java.loadClass('xaero.common.settings.ModOptions')
 
     PlayerEvents.tick(e => {
         if (e.player.age % 30) return
@@ -9,10 +11,20 @@
             fCaveModeAllowed = settings.class.getDeclaredField('caveMapsAllowed')
             fCaveModeAllowed.setAccessible(true)
         }
+        if (!settingsMinimap) {
+            settingsMinimap = Java.loadClass('xaero.minimap.XaeroMinimap').instance.settings
+            optionRadar = Java.loadClass('xaero.common.settings.ModOptions').RADAR_DISPLAYED
+        }
+
+        // cave mode
         let dimKey = String(e.level.dimensionKey.location())
         let whiteList = /* player.creative || */ dimKey == 'minecraft:the_nether' || dimKey.startsWith('undergarden')
         if (!whiteList && e.player.inventory.find('kubejs:sonic_mechanism') >= 0) whiteList = true
         e.player.enderChestInventory
         fCaveModeAllowed.set(settings, whiteList)
+
+        // radar mode
+        let hasRadar = e.player.inventory.find('kubejs:echoing_heart') >= 0
+        settingsMinimap.setOptionValue(minimapOptions.RADAR_DISPLAYED, hasRadar)
     })
 }
