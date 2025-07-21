cf8e89b7af9a80c3e1287cd63157aa124a3a7f8c
Gerardyang <1304274443@qq.com>
2025-07-21 15:13:03 +0000

    删除国际化，就这样吧。

--- kubejs/server_scripts/blind_challenger.js
+++ kubejs/server_scripts/blind_challenger.js
@@ -3,13 +3,15 @@ PlayerEvents.inventoryOpened((event) => {
   let {inventoryContainer, player, server} = event;
   // 这个 type 列表在这
   // https://lexxie.dev/forge/1.20.1/net/minecraft/world/inventory/MenuType.html
-  if (inventoryContainer.type == $MenuType.FURNACE || inventoryContainer.type == $MenuType.BLAST_FURNACE) {
-	// 3s 防抖
-	server.scheduleInTicks(20 * 3, () => {
-	  // [0] 原料槽，[1] 燃料槽，[2] 输出槽
-	  if (inventoryContainer.items[0] == Item.of("minecraft:raw_iron")) {
-	    server.runCommandSilent(`advancement grant ${player.getName().getString()} only path_of_truth:blind_challenger`);
-	  }
-	})
-  }
+  try {
+	if (inventoryContainer.type == $MenuType.FURNACE || inventoryContainer.type == $MenuType.BLAST_FURNACE) {
+	  // 3s 防抖
+	  server.scheduleInTicks(20 * 3, () => {
+	    // [0] 原料槽，[1] 燃料槽，[2] 输出槽
+	    if (inventoryContainer.items[0] == Item.of("minecraft:raw_iron")) {
+	      server.runCommandSilent(`advancement grant ${player.getName().getString()} only path_of_truth:blind_challenger`);
+	    }
+	  })
+    }
+  } catch(e) {}
 })
